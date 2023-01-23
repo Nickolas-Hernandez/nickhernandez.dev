3154c34a0f72184a5f8f69e0ecd04e6f6b35e363
Nick Hernandez <xnickhernandez@gmail.com>
2023-01-23 00:42:53 -0800

    Style text fields #3

--- src/components/introSection/introSection.tsx
+++ src/components/introSection/introSection.tsx
@@ -4,11 +4,12 @@ export const IntroSection = () => {
   return (
     <section className="intro-section">
       <div className="contain">
-        <p className="text-xl">Hi, my name is</p>
+        <p className="text-xl opacity-75">Hi, my name is</p>
         <h1 className="text-6xl font-bold">Nick Hernandez</h1>
         <h1 className="text-6xl font-normal">and I build on the web.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat quae nemo eligendi numquam alias?</p>
+        <p className="opacity-75 max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius fugiat quae nemo eligendi numquam alias?</p>
         <Link href="#contact"><button>Say Hello</button></Link>
+        <Link href="#contact"><button className="style-2">Say Hello</button></Link>
       </div>
     </section>
   );
