afae8dc2cac40b275ad3378d46456c2ba0724f41
Nick Hernandez <xnickhernandez@gmail.com>
2023-01-28 15:23:16 -0800

    Style and optimize for desktop #4

--- src/components/aboutMeSection/aboutMeSection.tsx
+++ src/components/aboutMeSection/aboutMeSection.tsx
@@ -7,7 +7,7 @@ export const AboutMeSection = () => {
     <section className="about-me-section">
       <div className="contain">
         <div className="biography">
-          <strong className="opacity-75 mb-5">About Me</strong>
+          <strong className="opacity-75 mb-5 text-sm lg:text-base">About Me</strong>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
             animi quae itaque magni eum ab? Aliquid esse quos impedit
@@ -18,29 +18,29 @@ export const AboutMeSection = () => {
           </p>
         </div>
         <div className="wrapper">
+          <div className="work-experience">
+            <strong className="text-sm lg:text-base opacity-70 mb-5">Work Experience</strong>
+            <div className="work-experience-item">
+              <div className="">
+                <p className="font-medium">Web Developer</p>
+                <p>Big Rig Media</p>
+              </div>
+              <div className="text-right">
+                <p className="font-medium opacity-70">Aug 2021 - Present</p>
+                <p className="opacity-70">{`${years} yr ${months} mos`}</p>
+              </div>
+            </div>
+          </div>
           <div className="education">
-            <strong className="text-sm opacity-75 mb-5">Education</strong>
+            <strong className="text-sm lg:text-base opacity-70 mb-5">Education</strong>
             <div className="education-item">
               <div>
                 <p className="font-medium">Web Development Program</p>
                 <p >LearningFuze</p>
               </div>
               <div className="text-right">
-                <p className="font-medium opacity-75">Graduated April 2021</p>
-                <p className="opacity-75">4 months</p>
-              </div>
-            </div>
-          </div>
-          <div className="work-experience">
-            <strong className="text-sm opacity-75 mb-5">Work Experience</strong>
-            <div className="work-experience-item">
-              <div className="">
-                <p className="font-medium">Web Developer</p>
-                <p className="opacity-75">Big Rig Media</p>
-              </div>
-              <div className="text-right">
-                <p className="font-medium opacity-75">Aug 2021 - Present</p>
-                <p className="opacity-75">{`${years} yr ${months} mos`}</p>
+                <p className="font-medium opacity-70">Graduated April 2021</p>
+                <p className="opacity-70">4 months</p>
               </div>
             </div>
           </div>
