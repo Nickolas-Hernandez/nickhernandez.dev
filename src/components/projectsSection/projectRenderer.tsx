b589bc5af16d34f3c47d864f51867c16008d35e1
Nick Hernandez <xnickhernandez@gmail.com>
2023-02-05 12:39:26 -0800

    Style fix #4

--- src/components/projectsSection/projectRenderer.tsx
+++ src/components/projectsSection/projectRenderer.tsx
@@ -18,27 +18,29 @@ const Project = ({ project, featured }: { project: project, featured: boolean } 
   const Icons = IconRenderer(project.tags);
 
   return (
-    <div className="project bg-grey-dark ">
+    <div className={`project bg-grey-dark ${ featured ? 'featured' : '' }`}>
       <div className="image-wrapper max-w-max">
         <Image src={project.image} alt={`${project.title} Logo`} width={859} height={560}/>
       </div>
-      <div className="content-wrapper">
-        { featured ? <strong className='text-neutral'>Featured Project</strong> : null}
-        <h3>{project.title}</h3>
-        <div className="icons-wrapper">
-          { ...Icons }
+      <div className="wrapper">
+        <div className="content-wrapper">
+          { featured ? <strong className='text-neutral'>Featured Project</strong> : null}
+          <h3>{project.title}</h3>
+          <div className="icons-wrapper">
+            { ...Icons }
+          </div>
+          <p className="md:mb-4">{project.description}</p>
         </div>
-        <p>{project.description}</p>
-      </div>
-      <div className="link-wrapper">
-        <Link href={ project.live } target="_blank">
-          <button className='style-project'>
-            <Image className='mr-2' src={ openLink } alt="Visit project site" width={18} height={18} /> Visit Site
-          </button>
-        </Link>
-        <Link href={ project.github } target="_blank">
-          <Image src={github} alt="Link to Github Repository" width={30} height={30} />
-        </Link>
+        <div className="link-wrapper">
+          <Link href={ project.live } target="_blank">
+            <button className='style-project'>
+              <Image className='mr-2' src={ openLink } alt="Visit project site" width={18} height={18} /> Visit Site
+            </button>
+          </Link>
+          <Link href={ project.github } target="_blank">
+            <Image src={github} alt="Link to Github Repository" width={30} height={30} />
+          </Link>
+        </div>
       </div>
     </div>
   );
