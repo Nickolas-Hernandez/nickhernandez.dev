49e3d7a6bd938c8fb79cfb5a8803c29c66f8bbbf
Nick Hernandez <xnickhernandez@gmail.com>
2023-02-19 21:52:36 -0800

    Corrections #11

--- src/components/layout/components/footer/footer.tsx
+++ src/components/layout/components/footer/footer.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import linkedin from "public/linkedin.svg";
-import github from "public/github.svg";
+import whiteGithub from "public/whiteGithub.svg";
 
 export const Footer = () => {
   return (
-    <footer className="text-gray-600 body-font pb-5">
+    <footer className="text-gray-600 body-font pb-5 pt-8 lg:pt-10">
       <div className="contain">
-        <div className="wrapper mb-4 md:mb-0">
-          <Link className="flex items-center md:mr-4" href="https://github.com/Nickolas-Hernandez" target="_blank" rel="noreffer"><Image src={github} className="mr-2" alt="Link to github proffile" width={20} height={20} />GitHub</Link>
-          <Link className="flex items-center" href="https://www.linkedin.com/in/nickolashernandez/" target="_blank" rel="noreffer"><Image src={linkedin} className="mr-2" alt="Link to Linkedin Page" width={20} height={20} />Linkedin</Link>
+        <div className="wrapper mb-2 md:mb-0">
+          <Link className="flex items-center justify-center md:justify-start  md:mr-4 mb-2 md:mb-0" href="https://github.com/Nickolas-Hernandez" target="_blank" rel="noreffer"><Image src={whiteGithub} className="mr-2" alt="Link to GitHub profile" width={16} height={16} />Gitub</Link>
+          <Link className="flex items-center justify-center md:justify-start" href="https://www.linkedin.com/in/nickolashernandez/" target="_blank" rel="noreffer"><Image src={linkedin} className="mr-2" alt="Link to Linkedin Page" width={16} height={16} />Linkedin</Link>
         </div>
         <Link className="flex items-center nic" href="https://www.nicmorey.com" target="_blank" rel="noreffer">Designed by Nic Morey</Link>
       </div>
