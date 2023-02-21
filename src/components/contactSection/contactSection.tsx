1631829e719fa64a515e27e725990dcdb4287587
Nick Hernandez <xnickhernandez@gmail.com>
2023-02-20 19:55:38 -0800

    Add unstyled form #14

--- src/components/contactSection/contactSection.tsx
+++ src/components/contactSection/contactSection.tsx
@@ -1,7 +1,31 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  firstName: string,
+  lastName: string,
+  email: string,
+  message: string,
+};
+
 export const ContactSection = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
   return (
     <section id="contact" className="contact-section" >
-      <div className="contain"></div>
+      <div className="contain">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" placeholder="First Name" {...register("firstName")} />
+          {errors.firstName && "First name is required"}
+          <input type="text" defaultValue="Last Name" {...register("firstName")} />
+          {errors.lastName && "Last name is required"}
+          <input type="email" defaultValue="Email Address" {...register("email")} />
+          {errors.email && "Email is required"}
+          <textarea id="" cols={30} rows={10} {...register("message")}></textarea>
+          {errors.message && "Message is required"}
+          <input type="submit" />
+        </form>
+      </div>
     </section>
   );
 };
