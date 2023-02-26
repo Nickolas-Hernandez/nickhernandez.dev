e0beae480b3afc118817f8efc2f101138f8cb3db
Nick Hernandez <xnickhernandez@gmail.com>
2023-02-25 17:15:02 -0800

    update cloner cube link #14

--- src/components/contactSection/contactSection.tsx
+++ src/components/contactSection/contactSection.tsx
@@ -14,17 +14,27 @@ export const ContactSection = () => {
   return (
     <section id="contact" className="contact-section" >
       <div className="contain">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input className="half-input" type="text" placeholder="First Name" {...register("firstName")} />
-          {errors.firstName && "First name is required"}
-          <input className="half-input" type="text" defaultValue="Last Name" {...register("firstName")} />
-          {errors.lastName && "Last name is required"}
-          <input type="email" defaultValue="Email Address" {...register("email")} />
-          {errors.email && "Email is required"}
-          <textarea id="" cols={30} rows={10} {...register("message")}></textarea>
-          {errors.message && "Message is required"}
-          <button className="w-full" type="submit">Submit</button>
-        </form>
+        <div className="contact-text-wrapper">
+          <h5>Get In Touch!</h5>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+            assumenda suscipit sit, pariatur minus rem dolores alias sunt
+            temporibus voluptates!
+          </p>
+        </div>
+        <div className="w-full lg:w-1/2 lg:px-4">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input className="half-input" type="text" placeholder="First Name" {...register("firstName")} />
+            {errors.firstName && "First name is required"}
+            <input className="half-input" type="text" placeholder="Last Name" {...register("firstName")} />
+            {errors.lastName && "Last name is required"}
+            <input type="email" placeholder="Email Address" {...register("email")} />
+            {errors.email && "Email is required"}
+            <textarea id="" placeholder="Message" {...register("message")}></textarea>
+            {errors.message && "Message is required"}
+            <button className="w-full" type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     </section>
   );
